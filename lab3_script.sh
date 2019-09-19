4200c72feb461c2e53739a0e73e4a0c3b48b1866
techLord-pm <pemi2427@colorado.edu>
2019-09-19 17:38:17 -0600

    asdf

--- lab3_script.sh
+++ lab3_script.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-#Authors : Peter Minwegen
+#Authors : Peter Minwegen, Isaac, Zach
 #Date: 9/19/2019
 
 #Problem 1 Code:
 #add pseudocode
 
+#readfilename
 echo "Enter a filename: "
 read fileName
 
+#read expression
 echo "Enter an expression: "
 read expression
 
-grep $expression $filename
+#navigate to file, begin searching for expression
+grep $expression $fileName
 
-grep -c '[0-9]\{3\}-[0-9]\{3}\-[0-9]\{4\}' $filename
-grep -c '@' $filename
-grep -o '303-[0-9]\{3\}\-[0-9]\{4\}' $filename
-grep '@geocities.com' $filename >> email_results.txt
-
+grep -c '[0-9]\{3\}-[0-9]\{3}\-[0-9]\{4\}' $fileName
+grep -c '@' $fileName
+grep -o '303-[0-9]\{3\}\-[0-9]\{4\}' $fileName
+grep '@geocities.com' $fileName >> email_results.txt
+#add results to email_results.txt
 
